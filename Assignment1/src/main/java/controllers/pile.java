3764fd709d26edc8005bcd501f20f4474ee6b738
Nburnettt <nathanburnett96@yahoo.com>
2016-01-21 20:34:46 -0800

    Create pile.java

---
+++ Assignment1/src/main/java/controllers/pile.java
@@ -0,0 +1,38 @@
+package controllers;
+
+/**
+ * Created by Natha_000 on 1/21/2016.
+ */
+public class game {
+    private int score = 0;
+    private deck myDeck;
+    public pile one;
+    public pile two;
+    public pile three;
+    public pile four;
+
+    game(){
+        myDeck = new deck();
+        one = new pile();
+        two = new pile();
+        three = new pile();
+        four = new pile();
+
+        deal();
+    }
+    public void deal(){
+        one.add(myDeck.dealRandom());
+        two.add(myDeck.dealRandom());
+        three.add(myDeck.dealRandom());
+        four.add(myDeck.dealRandom());
+    }
+
+
+    public int getScore(){
+        return score;
+    }
+    public int remainingCards(){
+        return myDeck.numCards();
+    }
+
+}
